549984a614de0117149d5acc25bee0eb14285e88
sunilkumar sajjan <sunilkumarsajjan777@gmail.com>
2023-02-28 11:24:50 +0530

    classroom assignment

---
+++ src/main/java/com/driver/StudentRepository.java
@@ -0,0 +1,85 @@
+package com.driver;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+@Component
+public class StudentRepository {
+
+    static HashMap<String,Student> studentdb =new HashMap<>();
+    static HashMap<String,Teacher> teacherDb=new HashMap<>();
+    static HashMap<Student,Teacher> studentTeacherDb =new HashMap<>();
+
+    public void addStudent(Student student){
+        studentdb.put(student.getName(),student);
+    }
+    public void addTeacher(Teacher teacher){
+        teacherDb.put(teacher.getName(),teacher);
+    }
+
+    public void addStudentTeacherPair(String student,String teacher){
+        studentTeacherDb.put(studentdb.get(student),teacherDb.get(teacher));
+    }
+
+    public Student getStudentByName(String name){
+       return studentdb.get(name);
+    }
+    public Teacher getTeacherByName(String name){
+        return teacherDb.get(name);
+    }
+
+
+    public List<String> getStudentsByTeacherName(String name){
+
+        List<String> listOfStudents = new ArrayList<>();
+        for(Student s: studentTeacherDb.keySet()){
+            if(studentTeacherDb.get(s).getName().equals(name))
+                listOfStudents.add(s.getName());
+        }
+        return listOfStudents;
+    }
+
+    public List<String> getAllStudents()
+    {
+        List<String> students=new ArrayList<>();
+
+        for (String student :studentdb.keySet() ){
+            students.add(student);
+        }
+        return students;
+    }
+
+    public static void deleteTeacherByName(String teacher){
+
+        teacherDb.remove(teacher);
+        List<String> students= new ArrayList<>();
+
+        for (Student s: studentTeacherDb.keySet())
+            if (studentTeacherDb.get(s).getName().equals(teacher))
+            {
+                students.add(s.getName());
+                studentTeacherDb.remove(s);
+            }
+
+        for (String s:students){
+            studentdb.remove(s);
+        }
+
+    }
+
+    public void deleteAllTeachers(){
+
+        for(Student s: studentTeacherDb.keySet()){
+            studentdb.remove(s.getName());
+        }
+
+        teacherDb.clear();
+        studentTeacherDb.clear();
+
+    }
+}
--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,13 +1,28 @@
 package com.driver;
 
 public class Teacher {
-
     private String name;
-
     private int numberOfStudents;
-
     private int age;
 
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
     public Teacher() {
 
     }
